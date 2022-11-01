3b0a6d55278ad6be4bf911f17ad0daa1fbc2216d
dmtsolovey <dmt.solovey@gmail.com>
2022-11-01 19:36:11 +0200

    make media print

--- src/components/content/Projects/Projects.jsx
+++ src/components/content/Projects/Projects.jsx
@@ -2,27 +2,33 @@ import "./Projects.css";
 
 const projects = [
   {
-    text: "https://stape.io",
+    text: "Stape website",
+    link: "https://stape.io",
     interface: "React, TypeScript, Next.js, SASS, i18n",
   },
   {
-    text: "https://app.stape.io",
+    text: "Stape admin-panel",
+    link: "https://app.stape.io",
     interface: "React, Next.js, TypeScript, SWR, StoryBook, SASS",
   },
   {
     text: "Trello Clone",
+    link: "https://github.com/dmtsolovey/trello-clone",
     interface: "React",
   },
   {
     text: "Pricing Page",
+    link: "https://github.com/dmtsolovey/pricing-page",
     interface: "HTML5, CSS3, JS ",
   },
   {
     text: "Coffee Shop",
+    link: "https://github.com/dmtsolovey/coffe-shop",
     interface: "React",
   },
   {
     text: "Todo List",
+    link: "https://github.com/dmtsolovey/todos",
     interface: "JavaScript",
   },
 ];
@@ -31,7 +37,12 @@ export default function Projects() {
   const listItem = projects.map((item) => (
     <li key={item.text} className="projects-item">
       <div className="projects-content">
-        <p className="projects-text">{item.text}</p>
+        <p className="projects-text">
+          {item.text} /{" "}
+          <a href={item.link} className="projects-link" title="Go to the link">
+            {item.link}
+          </a>
+        </p>
         <span className="projects-interface">
           <b>Used technologies: </b>
           {item.interface}
